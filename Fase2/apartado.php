708421910b0b878021453aa728457321512554ec
jesucr4 <jesucr4@users.noreply.github.com>
2020-11-12 11:21:08 +0100

    creación apartado.php

---
+++ Fase2/apartado.php
@@ -0,0 +1,31 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
+
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+
+<main>
+<section class="content">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-sm-12">
+        <h1>Deportes</h1>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <h1>Últimas noticas</h1>
+      </div>
+    </div>
+  </div>
+</section>
+</main>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
