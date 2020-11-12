23c6f686c4e76e068cacf2df585b82650c0a2cc4
irenelolie <irenelopezliebana@gmail.com>
2020-11-12 13:25:50 +0100

    footer con cambios

--- Fase2/includes/footer.php
+++ Fase2/includes/footer.php
@@ -1,5 +1,10 @@
 <footer class="footer">
   <div class="container">
+    <div class="footer container">
+    <a href="about.php">Sobre Nosotros</a>
+    <a href="#">Conoce al equipo</a>
+    <a href="contact.php">Contáctanos</a>
+    </div>
     <p>&copy; <?php print date("Y");?> TechnoTeam.</p>
   </div>
 </footer>
