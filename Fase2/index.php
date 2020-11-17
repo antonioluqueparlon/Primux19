9332f0322f4ce637d04dbb66ce6cc6e91d438350
antoniojespg <antoniojespg@gmail.com>
2020-11-17 12:28:15 +0100

    index boostrap

--- Fase2/index.php
+++ Fase2/index.php
@@ -6,18 +6,153 @@
 </head>
 <body>
 
-<?php include("includes/design-top.php");?>
-<?php include("includes/navigation.php");?>
+<?php //include("includes/navigation.php");?>
 
 
 <main>
-<section class="content">
-  <div class="full-bleed cool-photo">
-  </div>
-</section>
+<div class="container text-center">
+  
+<h1>Titulares</h1>
+    <div class="row">
+        <div class="col-sm-3 card">
+          <a href="noticia.php">
+            <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/71qkYMejD2L.jpg" style="width:100%">
+            <div class="card-body">
+              <h5 class="card-title">Titulo noticia 1</h5>
+              <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, laudantium.</p>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-3 card">
+          <a href="noticia.php">
+            <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/71qkYMejD2L.jpg" style="width:100%">
+            <div class="card-body">
+              <h5 class="card-title">Titulo noticia 2</h5>
+              <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, laudantium.</p>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-3 card">
+          <a href="noticia.php">
+            <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/71qkYMejD2L.jpg" style="width:100%">
+            <div class="card-body">
+              <h5 class="card-title">Titulo noticia 3</h5>
+              <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, laudantium.</p>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-3 card">
+          <a href="noticia.php">
+            <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/71qkYMejD2L.jpg" style="width:100%">
+            <div class="card-body">
+              <h5 class="card-title">Titulo noticia 4</h5>
+              <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, laudantium.</p>
+            </div>
+          </a>
+        </div>
+    </div>
+ </div>
+
+<h1>Noticias Destacadas </h1>
+
+    <div class="row">
+      <div class="col-sm-7 card">
+        <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/71qkYMejD2L.jpg" style="width:100%; height:75%">
+        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, laudantium.</p>
+      </div>
+
+
+      <div class="card col-sm-5">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/71qkYMejD2L.jpg" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/71qkYMejD2L.jpg" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/71qkYMejD2L.jpg" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+      </div>     
+
+    </div>
+</div>
+</div>
+<h1> Nuevas Noticias </h1>
+    <div class="row">
+      <div class="card mb-12">
+        <div class="row no-gutters">
+          <div class="col-sm-3">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/71qkYMejD2L.jpg" class="card-img img-fluid" alt="..." style="width: 65%">
+          </div>
+          <div class="col-sm-9">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="card mb-12">
+        <div class="row no-gutters">
+          <div class="col-sm-3">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/71qkYMejD2L.jpg" class="card-img" alt="..." style="width: 65%">
+          </div>
+          <div class="col-sm-9">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="card mb-12">
+        <div class="row no-gutters">
+          <div class="col-sm-3">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/71qkYMejD2L.jpg" class="card-img" alt="..." style="width: 65%">
+          </div>
+          <div class="col-sm-9">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+</div>
 </main>
 
-<?php include("includes/footer.php");?>
+<?php //include("includes/footer.php");?>
 
 </body>
 </html>
