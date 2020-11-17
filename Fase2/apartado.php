b852fb3df4cc4485bb134bb4201071c39076538e
jesucr4 <jesucr4@users.noreply.github.com>
2020-11-17 12:23:09 +0100

    17-11-20

--- Fase2/apartado.php
+++ Fase2/apartado.php
@@ -15,17 +15,17 @@
 <section class="content">
   <div class="container">
     <div class="row">
-      <div class="col-md-8 col-sm-12">
+      <div class="col-lg-8 col-sm-12">
         <h1>APARTADO</h1>
           <div class="card img-fluid" >
-            <img class="card-img-top" src="http://lorempixel.com/output/sports-q-c-320-180-9.jpg" alt="Card image" >
+            <img class="card-img-top" src="http://lorempixel.com/output/sports-q-c-320-180-9.jpg" alt="Card image" id="imgTop" >
             <div class="card-img-overlay d-flex align-items-end">
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit exercitationem rerum dolorem quas consectetur, deleniti ad veniam quidem eligendi et impedit, dignissimos placeat doloremque laboriosam quam nam at quod tempore!</p>
+              <p class="card-text font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit exercitationem rerum dolorem quas consectetur, deleniti ad veniam quidem eligendi et impedit, dignissimos placeat doloremque laboriosam quam nam at quod tempore!</p>
             </div>
           </div>
       </div>
-      <div class="col-md-4">
-        <h1>Últimas noticas</h1>
+      <div class="col-lg-4">
+        <h1>Últimas noticias</h1>
         <div class="row">
           <div class="col-md-12">
             <div class="card bg-light">
