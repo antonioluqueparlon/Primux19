f35dd4667390bba2f2b2375b3b4b36d83c9a6e3e
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2020-11-19 10:45:13 +0100

    Actualizado 19/11

--- Fase2/includes/navigation.php
+++ Fase2/includes/navigation.php
@@ -1,15 +1,40 @@
-<nav class="main-nav">
-  <div class="container">
-    <ul>
-      <li class="mobile-button"><a href="#">Menu</a></li>
-    <li><a href="about.php">About Us</a></li>
-    <li><a href="work.php">Work</a></li>
-    <li><a href="methods.php">Methods</a></li>
-    <li><a href="results.php">Results</a></li>
-    <li><a href="contact.php">Contact</a></li>
-  </ul>
+
+<nav class="navbar navbar-expand-xl navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php"><img src="img/Logo.JPG"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse navText" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+        <p class="aNav">Tecnología</p>
+          </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+        <p class="aNav">Deportes</p>
+          </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <p class="aNav">Motor</p></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <p class="aNav">VideoJuegos</p>
+          </a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="">
+      <button class="btn my-2 my-sm-0 btnNav" type="submit">Buscar</button>
+    </form>
+    <a href="login.php"><button class="btn  my-2 my-sm-0 btnNav">Entrar</button></a>
   </div>
 </nav>
+
 <!--
 <div class="container">
 	<ul class="nav nav-pills">
