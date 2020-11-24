e964c886533487b3c50577f8a9f20f4c5a816505
irenelolie <irenelopezliebana@gmail.com>
2020-11-24 11:03:30 +0100

    cambios en seccion

--- Fase2/seccion.php
+++ Fase2/seccion.php
@@ -14,58 +14,64 @@
 
 
     <main>
-        <section class="content">
-            <div class="col-md-12">
-                <h1>Últimas noticias</h1>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div  class="card bg-secondary text-white">
-                            <div class="card-body text-center">
-                                <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg">
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit egestas</p>
+        <div class="seccion">
+            <section class="content">
+                <div class="col-md-12">
+                    <h1>Últimas noticias</h1>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card bg-secondary text-white">
+                                <div class="card-body text-center">
+                                    <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg">
+                                    <span class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra, cursus natoque dignissim molestie donec lectus ligula sollicitudin, primis est penatibus gravida euismod aliquet venenatis. Per ultricies dictumst luctus donec nostra sagittis convallis dictum, ultrices curabitur orci consequat pharetra felis nullam cum ligula, augue condimentum ullamcorper vel senectus curae et. Vivamus nullam maecenas torquent congue sociosqu magnis tortor massa nascetur taciti non faucibus, ligula lacinia tristique inceptos proin lectus habitant cras enim tellus eros. Pellentesque volutpat ullamcorper habitant malesuada posuere risus commodo ad, bibendum metus molestie rutrum rhoncus facilisi natoque massa, neque auctor leo dapibus urna dis cubilia nam, curae at est scelerisque habitasse ligula feugiat.</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card bg-secondary text-white">
+                                <div class="card-body text-center">
+                                    <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg">
+                                    <span class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra, cursus natoque dignissim molestie donec lectus ligula sollicitudin, primis est penatibus gravida euismod aliquet venenatis. Per ultricies dictumst luctus donec nostra sagittis convallis dictum, ultrices curabitur orci consequat pharetra felis nullam cum ligula, augue condimentum ullamcorper vel senectus curae et. Vivamus nullam maecenas torquent congue sociosqu magnis tortor massa nascetur taciti non faucibus, ligula lacinia tristique inceptos proin lectus habitant cras enim tellus eros. Pellentesque volutpat ullamcorper habitant malesuada posuere risus commodo ad, bibendum metus molestie rutrum rhoncus facilisi natoque massa, neque auctor leo dapibus urna dis cubilia nam, curae at est scelerisque habitasse ligula feugiat.</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card bg-secondary text-white">
+                                <div class="card-body text-center">
+                                    <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg">
+                                    <span class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra, cursus natoque dignissim molestie donec lectus ligula sollicitudin, primis est penatibus gravida euismod aliquet venenatis. Per ultricies dictumst luctus donec nostra sagittis convallis dictum, ultrices curabitur orci consequat pharetra felis nullam cum ligula, augue condimentum ullamcorper vel senectus curae et. Vivamus nullam maecenas torquent congue sociosqu magnis tortor massa nascetur taciti non faucibus, ligula lacinia tristique inceptos proin lectus habitant cras enim tellus eros. Pellentesque volutpat ullamcorper habitant malesuada posuere risus commodo ad, bibendum metus molestie rutrum rhoncus facilisi natoque massa, neque auctor leo dapibus urna dis cubilia nam, curae at est scelerisque habitasse ligula feugiat.</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card bg-secondary text-white">
+                                <div class="card-body text-center">
+                                    <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg">
+                                    <span class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra, cursus natoque dignissim molestie donec lectus ligula sollicitudin, primis est penatibus gravida euismod aliquet venenatis. Per ultricies dictumst luctus donec nostra sagittis convallis dictum, ultrices curabitur orci consequat pharetra felis nullam cum ligula, augue condimentum ullamcorper vel senectus curae et. Vivamus nullam maecenas torquent congue sociosqu magnis tortor massa nascetur taciti non faucibus, ligula lacinia tristique inceptos proin lectus habitant cras enim tellus eros. Pellentesque volutpat ullamcorper habitant malesuada posuere risus commodo ad, bibendum metus molestie rutrum rhoncus facilisi natoque massa, neque auctor leo dapibus urna dis cubilia nam, curae at est scelerisque habitasse ligula feugiat.</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card bg-secondary text-white">
+                                <div class="card-body text-center">
+                                    <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg">
+                                    <span class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra, cursus natoque dignissim molestie donec lectus ligula sollicitudin, primis est penatibus gravida euismod aliquet venenatis. Per ultricies dictumst luctus donec nostra sagittis convallis dictum, ultrices curabitur orci consequat pharetra felis nullam cum ligula, augue condimentum ullamcorper vel senectus curae et. Vivamus nullam maecenas torquent congue sociosqu magnis tortor massa nascetur taciti non faucibus, ligula lacinia tristique inceptos proin lectus habitant cras enim tellus eros. Pellentesque volutpat ullamcorper habitant malesuada posuere risus commodo ad, bibendum metus molestie rutrum rhoncus facilisi natoque massa, neque auctor leo dapibus urna dis cubilia nam, curae at est scelerisque habitasse ligula feugiat.</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div  class="card bg-secondary text-white">
-                            <div class="card-body text-center">
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit egestas</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div  class="card bg-secondary text-white">
-                            <div class="card-body text-center">
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit egestas</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div  class="card bg-secondary text-white">
-                            <div class="card-body text-center">
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit egestas</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div  class="card bg-secondary text-white">
-                            <div class="card-body text-center">
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit egestas</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
-            </div>
+        </div>
+        </div>
+        </div>
         </section>
     </main>
 
