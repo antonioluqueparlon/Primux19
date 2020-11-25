b5dbf01e0fc667a7da56bd34fd289479eca5f13e
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2020-11-25 10:13:09 +0100

    25/11 v1

--- Fase2/about.php
+++ Fase2/about.php
@@ -33,33 +33,31 @@
 			<div class="row justify-content-center">
 				<div class="col-md-12 col-lg-10">
 					<h3 class="mbr-section-title mbr-fonts-style mb-4 display-5">
-						<strong>Preguntas frecuentes</strong></h3>
+						<strong id="p1">Preguntas frecuentes</strong></h3>
 				</div>
 			</div>
 			<div class="row justify-content-center">
 				<div class="col-md-12 col-lg-10">
 					<div class="row justify-content-center">
 						<div class="col-12 col-lg-6">
-							<ul class="list mbr-fonts-style display-7">
-								<li><strong>How to create more items in this list?</strong><br>Press <strong>Enter </strong>to create a new
-									bullet. To
-									create an answer for a question, press <strong>Shift+Enter</strong>.</li>
-								<li><strong>Can I create sites for commerical projects?</strong><br>Yes, Mobirise is free for both
-									non-profit
-									and commercial sites.</li>
-								<li><strong>How to install more extensions?</strong><br>Open the main menu in the app and find the
-									Extensions
-									tab. Click on it to open the Extensions list.</li>
+							<ul class="">
+								<li class="l1class"><strong>¿Por qué hemos elegido este formato de pagina?</strong><br>Nos vemos
+									capaces de llevar una revista como esta donde la gente podra esta al dia de todo y donde podran loguearse
+									y comentar con otra gente que tenga los mismos gustos que nosotros.</li>
+								<li><strong>¿Podemos participar y colaborar con la página?</strong><br>Sí, TrendTop es en este momento esta abierto a todo tipo de ayuda de gente
+								que colabore publicando noticias, redactando y llevando los comentarios,etc.</li>
+								<li><strong>¿Como podemos ponernos en contacto con vosotros?</strong><br>A pie de pagina tenemos un formulario de contacto para todas vuestras dudas</li>
 							</ul>
 						</div>
 						<div class="col-12 col-lg-6">
 							<ul class="list mbr-fonts-style display-7">
-								<li><strong>What is Mobirise Kit?
-									</strong><br>Mobirise Kit is a service that provides the access to all current and new
-									themes/extensions developed by Mobirise.&nbsp;</li>
-								<li><strong>What is Code Editor?<br></strong>This extension allows editing the code of block in the
-									app. Also,
-									it's possible to add code to the head and body parts of pages.</li>
+								<li><strong>Qué es Trend Top?
+									</strong><br>Es una revista de informacion actualizada al año 2020.&nbsp;</li>
+								<li><strong>¿Qué es lo que nos ha llevado a estudiar DAW? <br></strong>Nos apasiona el mundo de la informatica y es mundo lleno
+								de muchas posibilidades y nos gusta crear contenido para los clientes. .</li>
+								<li><strong>¿Y por qué en Lucena? <br></strong>Lucena es una localidad cercana y que tiene la posibilidad de impartir este ciclo, lo cual hace que
+								la tengamos muy cerca de los pueblos de alrededor y que nos facilite el ir y venir a muchos de nosotros
+								que somos de fuera de los pueblos de alrededor .</li>
 							</ul>
 						</div>
 					</div>
--- Fase2/includes/head-tag-contents.php
+++ Fase2/includes/head-tag-contents.php
@@ -1,22 +1,24 @@
-<title><?php print $PAGE_TITLE;?></title>
+<title><?php print $PAGE_TITLE; ?></title>
 
 <?php if ($CURRENT_PAGE == "Index") { ?>
-	<meta name="description" content="" />
-	<meta name="keywords" content="" /> 
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
 <?php } ?>
 
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width">
 <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;1,700&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Arvo:wght@700&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;1,700&display=swap" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Arvo:wght@700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="scss/mystyle.css">
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="scss/index.css">
@@ -24,7 +26,3 @@
 <link rel="stylesheet" href="scss/styleContacto.css">
 <link rel="stylesheet" href="scss/styleSeccion.css">
 <link rel="stylesheet" href="scss/styleAbout.css">
-
-
-
-  
