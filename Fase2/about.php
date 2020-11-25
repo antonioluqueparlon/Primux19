f0630a993b7bd1424c9b6a7e1a76a37a3f4cdcff
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2020-11-25 18:38:52 +0100

    25/11 about casi terminado

--- Fase2/about.php
+++ Fase2/about.php
@@ -11,23 +11,17 @@
     <?php include("includes/navigation.php"); ?>
 
     <section class="content4 cid-sdGXGHOAx1" id="content4-2k">
-
-        <div class="mbr-overlay" style="opacity: 0.7;">
-        </div>
-        <div class="container-fluid">
-            <div class="row justify-content-center">
-                <div class="title col-md-12 col-lg-10">
-                    <h3 class="mbr-section-title mbr-fonts-style align-center mb-4 display-2">
-                        <strong id="p1" class="p1class">Sobre Trend Top</strong></h3>
-                    <h4 class="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">Somos una revista de informacion actualizada sobre las diferentes secciones mas importantes del mundo de la actualidad(Deportes, Tecnología,Motor,Videojuegos...).
-                        Somos un grupo de alumnos de 2ºDAW del I.E.S Marques de Comares (Lucena-Córdoba) conformado por Antonio Pérez, Jesús Cano, Irene López y Antonio Luque</h4>
-
-                </div>
+    <h1 id="p1">Sobre Trend Top</h1>
+        <div class="card" style="mbr-overlay">
+            <img class="card-img-top imagenAbout" src="img/imagenfondo.jpg" alt="Card image">
+            <div class="card-img-overlay">
+                <h4 class="textoFoto">Somos una revista de informacion actualizada sobre las diferentes secciones mas importantes del mundo de la actualidad(Deportes, Tecnología,Motor,Videojuegos...).
+                    Somos un grupo de alumnos de 2ºDAW del I.E.S Marques de Comares (Lucena-Córdoba) conformado por Antonio Pérez, Jesús Cano, Irene López y Antonio Luque</h4>
             </div>
         </div>
     </section>
 
-    <section>
+    <section class="preguntas">
 
         <div class="container">
             <div class="row justify-content-center">
@@ -37,7 +31,7 @@
                 </div>
             </div>
             <div class="row justify-content-center">
-                <div class="col-md-12 col-lg-10">
+                <div class="col-md-12">
                     <div class="row justify-content-center">
                         <div class="col-12 col-lg-6">
                             <ul class="">
@@ -45,11 +39,11 @@
                                     <element class="antonio"><strong>¿Por qué hemos elegido este formato de pagina?</strong><br>Nos vemos
                                         capaces de llevar una revista como esta donde la gente podra esta al dia de todo y donde podran loguearse
                                         y comentar con otra gente que tenga los mismos gustos que nosotros.</element>
-                                </li>
+                                </li><br>
                                 <li>
                                     <element class="antonio"><strong>¿Podemos participar y colaborar con la página?</strong><br>Sí, TrendTop es en este momento esta abierto a todo tipo de ayuda de gente
                                         que colabore publicando noticias, redactando y llevando los comentarios,etc.</element>
-                                </li>
+                                </li><br>
                                 <li>
                                     <element class="antonio"><strong>¿Como podemos ponernos en contacto con vosotros?</strong><br>A pie de pagina tenemos un formulario de contacto para todas vuestras dudas</element>
                                 </li>
@@ -60,15 +54,16 @@
                                 <li>
                                     <element class="antonio"><strong>Qué es Trend Top?
                                         </strong><br>Es una revista de informacion actualizada al año 2020.&nbsp;</element>
-                                </li>
+                                </li><br>
                                 <li>
                                     <element class="antonio"><strong>¿Qué es lo que nos ha llevado a estudiar DAW? <br></strong>Nos apasiona el mundo de la informatica y es mundo lleno
                                         de muchas posibilidades y nos gusta crear contenido para los clientes.</element>
-                                </li>
+                                </li><br>
                                 <li>
-                                <element class="antonio"><strong>¿Y por qué en Lucena? <br></strong>Lucena es una localidad cercana y que tiene la posibilidad de impartir este ciclo, lo cual hace que
-                                    la tengamos muy cerca de los pueblos de alrededor y que nos facilite el ir y venir a muchos de nosotros
-                                    que somos de fuera de los pueblos de alrededor .</element></li>
+                                    <element class="antonio"><strong>¿Y por qué en Lucena? <br></strong>Lucena es una localidad cercana y que tiene la posibilidad de impartir este ciclo, lo cual hace que
+                                        la tengamos muy cerca de los pueblos de alrededor y que nos facilite el ir y venir a muchos de nosotros
+                                        que somos de fuera de los pueblos de alrededor .</element>
+                                </li><br>
                             </ul>
                         </div>
                     </div>
@@ -242,6 +237,104 @@
                                 <div class="card-body text-center mt-4">
                                     <h4 class="card-title">Antonio Luque</h4>
                                     <p class="card-text">This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.</p>
+                                    <ul class="list-inline">
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
+                                                <i class="fa fa-facebook"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
+                                                <i class="fa fa-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
+                                                <i class="fa fa-skype"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
+                                                <i class="fa fa-google"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ./Team member -->
+
+            <!-- Team member -->
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                    <div class="mainflip">
+                        <div class="frontside">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_04.jpg" alt="card image"></p>
+                                    <h4 class="card-title">Email</h4>
+                                    <p class="card-text">This is basic card with image on top, title, description and button.</p>
+                                    <a href="https://www.fiverr.com/share/qb8D02" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="backside">
+                            <div class="card">
+                                <div class="card-body text-center mt-4">
+                                    <h4 class="card-title">Email</h4>
+                                    <p class="card-text">TrendTop@gmail.com.</p>
+                                    <ul class="list-inline">
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
+                                                <i class="fa fa-facebook"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
+                                                <i class="fa fa-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
+                                                <i class="fa fa-skype"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
+                                                <i class="fa fa-google"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ./Team member -->
+
+            <!-- Team member -->
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                    <div class="mainflip">
+                        <div class="frontside">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_04.jpg" alt="card image"></p>
+                                    <h4 class="card-title">Telefono</h4>
+                                    <p class="card-text">Para contactar con nosotros en esta tarjeta se encuentra el numero de teléfono.</p>
+                                    <a href="https://www.fiverr.com/share/qb8D02" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="backside">
+                            <div class="card">
+                                <div class="card-body text-center mt-4">
+                                    <h4 class="card-title">Telefono</h4>
+                                    <p class="card-text">TLF: 957112233.</p>
                                     <ul class="list-inline">
                                         <li class="list-inline-item">
                                             <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
