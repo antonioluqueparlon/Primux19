797e61ae228465822e4bdd453644175780d3de04
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2020-11-25 01:39:16 +0100

    v1 about us

--- Fase2/about.php
+++ Fase2/about.php
@@ -1,27 +1,142 @@
-<?php include("includes/a_config.php");?>
+<?php include("includes/a_config.php"); ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<?php include("includes/head-tag-contents.php");?>
+	<?php include("includes/head-tag-contents.php"); ?>
 </head>
+
 <body>
 
-<?php include("includes/design-top.php");?>
-<?php include("includes/navigation.php");?>
+	<?php include("includes/navigation.php"); ?>
 
-<div class="container" id="main-content">
-	<h2>About Us</h2>
-	<p>"About Us" conten goes here.  I'll stick with teh "lorem ipsum" as well, so that the footer isn't immediately following this text.</p>
+	<section class="content4 cid-sdGXGHOAx1" id="content4-2k">
 
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
-</div>
+		<div class="mbr-overlay" style="opacity: 0.7; background-color: rgb(255, 255, 255);">
+		</div>
+		<div class="container-fluid">
+			<div class="row justify-content-center">
+				<div class="title col-md-12 col-lg-10">
+					<h3 class="mbr-section-title mbr-fonts-style align-center mb-4 display-2">
+						<strong id="p1" class="p1class">Sobre Trend Top</strong></h3>
+					<h4 class="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">Somos una revista de informacion actualizada sobre las diferentes secciones mas importantes del mundo de la actualidad(Deportes, Tecnología,Motor,Videojuegos...).
+						Somos un grupo de alumnos de 2ºDAW del I.E.S Marques de Comares (Lucena-Córdoba) conformado por Antonio Pérez, Jesús Cano, Irene López y Antonio Luque</h4>
 
-<?php include("includes/footer.php");?>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section>
+
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-12 col-lg-10">
+					<h3 class="mbr-section-title mbr-fonts-style mb-4 display-5">
+						<strong>Preguntas frecuentes</strong></h3>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-12 col-lg-10">
+					<div class="row justify-content-center">
+						<div class="col-12 col-lg-6">
+							<ul class="list mbr-fonts-style display-7">
+								<li><strong>How to create more items in this list?</strong><br>Press <strong>Enter </strong>to create a new
+									bullet. To
+									create an answer for a question, press <strong>Shift+Enter</strong>.</li>
+								<li><strong>Can I create sites for commerical projects?</strong><br>Yes, Mobirise is free for both
+									non-profit
+									and commercial sites.</li>
+								<li><strong>How to install more extensions?</strong><br>Open the main menu in the app and find the
+									Extensions
+									tab. Click on it to open the Extensions list.</li>
+							</ul>
+						</div>
+						<div class="col-12 col-lg-6">
+							<ul class="list mbr-fonts-style display-7">
+								<li><strong>What is Mobirise Kit?
+									</strong><br>Mobirise Kit is a service that provides the access to all current and new
+									themes/extensions developed by Mobirise.&nbsp;</li>
+								<li><strong>What is Code Editor?<br></strong>This extension allows editing the code of block in the
+									app. Also,
+									it's possible to add code to the head and body parts of pages.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="nosotros">
+		<div class="card cardNosotros">
+			<a href="#" class="enlaceNoticia">
+				<div class="row no-gutters">
+					<div class="col-md-4">
+						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFGi3dVsGtzx7EGSC8-GK7OSgaScqlAa396g&usqp=CAU" class="card-img" alt="...">
+					</div>
+					<div class="col-md-8">
+						<div class="card-body">
+							<h3 class="card-title indexNoticiaTitulo">Antonio Perez</h3>
+							<p class="card-text indexNoticiaTimer"><small class="text-muted">Last updated 9 mins ago</small></p>
+							<p class="card-text aboutTexto">Hablar sobre nosotros.</p>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<div class="card cardNosotros">
+			<a href="#" class="enlaceNoticia">
+				<div class="row no-gutters">
+					<div class="col-md-4">
+						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFGi3dVsGtzx7EGSC8-GK7OSgaScqlAa396g&usqp=CAU" class="card-img" alt="...">
+					</div>
+					<div class="col-md-8">
+						<div class="card-body">
+							<h3 class="card-title indexNoticiaTitulo">Irene Lopez</h3>
+							<p class="card-text indexNoticiaTimer"><small class="text-muted">Last updated 9 mins ago</small></p>
+							<p class="card-text aboutTexto">Hablar sobre nosotros.</p>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<div class="card cardNosotros">
+			<a href="#" class="enlaceNoticia">
+				<div class="row no-gutters">
+					<div class="col-md-4">
+						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFGi3dVsGtzx7EGSC8-GK7OSgaScqlAa396g&usqp=CAU" class="card-img" alt="...">
+					</div>
+					<div class="col-md-8">
+						<div class="card-body">
+							<h3 class="card-title indexNoticiaTitulo">Jesús Cano</h3>
+							<p class="card-text indexNoticiaTimer"><small class="text-muted">Last updated 9 mins ago</small></p>
+							<p class="card-text aboutTexto">Hablar sobre nosotros.</p>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<div class="card cardNosotros">
+			<a href="#" class="enlaceNoticia">
+				<div class="row no-gutters">
+					<div class="col-md-4">
+						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFGi3dVsGtzx7EGSC8-GK7OSgaScqlAa396g&usqp=CAU" class="card-img" alt="...">
+					</div>
+					<div class="col-md-8">
+						<div class="card-body">
+							<h3 class="card-title indexNoticiaTitulo">Antonio Luque Parlón</h3>
+							<p class="card-text indexNoticiaTimer"><small class="text-muted">Last updated 9 mins ago</small></p>
+							<p class="card-text aboutTexto">Hablar sobre nosotros.</p>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+	</section>
+
+	<?php include("includes/footer.php"); ?>
 
 </body>
+
 </html>
