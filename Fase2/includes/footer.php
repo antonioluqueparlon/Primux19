30ed1f072f880797387564376fdb88ba45e3455e
irenelolie <irenelopezliebana@gmail.com>
2020-11-26 12:29:20 +0100

    footer más pequeño

--- Fase2/includes/footer.php
+++ Fase2/includes/footer.php
@@ -1,18 +1,19 @@
 <div class="clear"></div>
 <footer class="bg-dark text-center">
   <div class="footer">
-    <div class="d-flex justify-content-around mb-2">
+    <div class="d-flex justify-content-around ">
     <div class="p-2"><a href="about.php">Sobre Nosotros</a></div>
     <div class="p-2"><a href="#">Conoce al equipo</a></div>
     <div class="p-2"><a href="contacto.php">Contáctanos</a></div>
     </div>
     <div>
-    <a href="#"><img src="https://www.flaticon.es/svg/static/icons/svg/1177/1177585.svg"/></a>
-    <a href="#"><img src="https://www.flaticon.es/svg/static/icons/svg/145/145812.svg"/></a>
-    <a href="#"><img src="https://www.flaticon.es/premium-icon/icons/svg/2504/2504792.svg"/></a>
-    <a href="#"><img src="https://www.flaticon.es/svg/static/icons/svg/747/747627.svg"/></a>
+    <a href="#"  class="ml-3"><img src="https://www.flaticon.es/svg/static/icons/svg/1177/1177585.svg"/></a>
+    <a href="#"  class="ml-3"><img src="https://www.flaticon.es/svg/static/icons/svg/145/145812.svg"/></a>
+    <span  class="ml-3">&copy; <?php print date("Y");?> TechnoTeam.</span>
+    <a href="#"  class="ml-3"><img src="https://www.flaticon.es/premium-icon/icons/svg/2504/2504792.svg"/></a>
+    <a href="#"  class="ml-3"><img src="https://www.flaticon.es/svg/static/icons/svg/747/747627.svg"/></a>
     </div>
     
   </div>
-  <div class="nombre"><p>&copy; <?php print date("Y");?> TechnoTeam.</p></div>
+  <div class="nombre mt-2"></div>
 </footer>
