92d6a7d1e1bf464aec58719eb104a0d3cc3439c7
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2020-11-26 11:23:08 +0100

    footer colores

--- Fase2/includes/footer.php
+++ Fase2/includes/footer.php
@@ -2,9 +2,9 @@
 <footer class="bg-dark text-center">
   <div class="footer">
     <div class="d-flex justify-content-around mb-2">
-    <div class="p-2"><a href="about.php">Sobre Nosotros</a></div>
-    <div class="p-2"><a href="#">Conoce al equipo</a></div>
-    <div class="p-2"><a href="contacto.php">Contáctanos</a></div>
+    <div class="p-2"><a class="footer" href="about.php">Sobre Nosotros</a></div>
+    <div class="p-2"><a class="footer" href="#">Conoce al equipo</a></div>
+    <div class="p-2"><a class="footer" href="contacto.php">Contáctanos</a></div>
     </div>
     <div>
     <a href="#"><img src="https://www.flaticon.es/svg/static/icons/svg/1177/1177585.svg"/></a>
