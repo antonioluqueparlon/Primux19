9188c3603c8da0b437233bfe25fc06fb71297bbc
irenelolie <irenelopezliebana@gmail.com>
2020-11-26 11:17:23 +0100

    seccion

--- Fase2/seccion.php
+++ Fase2/seccion.php
@@ -8,7 +8,7 @@
 
 <body>
 
-    <?php include("includes/design-top.php"); ?>
+    <!--<?php //include("includes/design-top.php"); ?>-->
     <?php include("includes/navigation.php"); ?>
     <?php include("includes/subNavigation.php"); ?>
 
@@ -20,9 +20,10 @@
                     <h1>Últimas noticias</h1>
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="card bg-secondary text-white">
+                            <div class="card">
                                 <div class="card-body text-center">
                                     <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg">
+                                    <h4>Título de noticia</h4>
                                     <span class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra, cursus natoque dignissim molestie donec lectus ligula sollicitudin, primis est penatibus gravida euismod aliquet venenatis. Per ultricies dictumst luctus donec nostra sagittis convallis dictum, ultrices curabitur orci consequat pharetra felis nullam cum ligula, augue condimentum ullamcorper vel senectus curae et. Vivamus nullam maecenas torquent congue sociosqu magnis tortor massa nascetur taciti non faucibus, ligula lacinia tristique inceptos proin lectus habitant cras enim tellus eros. Pellentesque volutpat ullamcorper habitant malesuada posuere risus commodo ad, bibendum metus molestie rutrum rhoncus facilisi natoque massa, neque auctor leo dapibus urna dis cubilia nam, curae at est scelerisque habitasse ligula feugiat.</span>
                                 </div>
                             </div>
@@ -30,9 +31,10 @@
                     </div>
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="card bg-secondary text-white">
+                            <div class="card">
                                 <div class="card-body text-center">
                                     <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg">
+                                    <h4>Título de noticia</h4>
                                     <span class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra, cursus natoque dignissim molestie donec lectus ligula sollicitudin, primis est penatibus gravida euismod aliquet venenatis. Per ultricies dictumst luctus donec nostra sagittis convallis dictum, ultrices curabitur orci consequat pharetra felis nullam cum ligula, augue condimentum ullamcorper vel senectus curae et. Vivamus nullam maecenas torquent congue sociosqu magnis tortor massa nascetur taciti non faucibus, ligula lacinia tristique inceptos proin lectus habitant cras enim tellus eros. Pellentesque volutpat ullamcorper habitant malesuada posuere risus commodo ad, bibendum metus molestie rutrum rhoncus facilisi natoque massa, neque auctor leo dapibus urna dis cubilia nam, curae at est scelerisque habitasse ligula feugiat.</span>
                                 </div>
                             </div>
@@ -40,9 +42,10 @@
                     </div>
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="card bg-secondary text-white">
+                            <div class="card">
                                 <div class="card-body text-center">
                                     <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg">
+                                    <h4>Título de noticia</h4>
                                     <span class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra, cursus natoque dignissim molestie donec lectus ligula sollicitudin, primis est penatibus gravida euismod aliquet venenatis. Per ultricies dictumst luctus donec nostra sagittis convallis dictum, ultrices curabitur orci consequat pharetra felis nullam cum ligula, augue condimentum ullamcorper vel senectus curae et. Vivamus nullam maecenas torquent congue sociosqu magnis tortor massa nascetur taciti non faucibus, ligula lacinia tristique inceptos proin lectus habitant cras enim tellus eros. Pellentesque volutpat ullamcorper habitant malesuada posuere risus commodo ad, bibendum metus molestie rutrum rhoncus facilisi natoque massa, neque auctor leo dapibus urna dis cubilia nam, curae at est scelerisque habitasse ligula feugiat.</span>
                                 </div>
                             </div>
@@ -50,9 +53,10 @@
                     </div>
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="card bg-secondary text-white">
+                            <div class="card">
                                 <div class="card-body text-center">
                                     <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg">
+                                    <h4>Título de noticia</h4>
                                     <span class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra, cursus natoque dignissim molestie donec lectus ligula sollicitudin, primis est penatibus gravida euismod aliquet venenatis. Per ultricies dictumst luctus donec nostra sagittis convallis dictum, ultrices curabitur orci consequat pharetra felis nullam cum ligula, augue condimentum ullamcorper vel senectus curae et. Vivamus nullam maecenas torquent congue sociosqu magnis tortor massa nascetur taciti non faucibus, ligula lacinia tristique inceptos proin lectus habitant cras enim tellus eros. Pellentesque volutpat ullamcorper habitant malesuada posuere risus commodo ad, bibendum metus molestie rutrum rhoncus facilisi natoque massa, neque auctor leo dapibus urna dis cubilia nam, curae at est scelerisque habitasse ligula feugiat.</span>
                                 </div>
                             </div>
@@ -60,9 +64,10 @@
                     </div>
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="card bg-secondary text-white">
+                            <div class="card">
                                 <div class="card-body text-center">
                                     <img src="https://miro.medium.com/max/1024/1*vxjAHkrXbGG6gOiPZgjeZA.jpeg">
+                                    <h4>Título de noticia</h4>
                                     <span class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra, cursus natoque dignissim molestie donec lectus ligula sollicitudin, primis est penatibus gravida euismod aliquet venenatis. Per ultricies dictumst luctus donec nostra sagittis convallis dictum, ultrices curabitur orci consequat pharetra felis nullam cum ligula, augue condimentum ullamcorper vel senectus curae et. Vivamus nullam maecenas torquent congue sociosqu magnis tortor massa nascetur taciti non faucibus, ligula lacinia tristique inceptos proin lectus habitant cras enim tellus eros. Pellentesque volutpat ullamcorper habitant malesuada posuere risus commodo ad, bibendum metus molestie rutrum rhoncus facilisi natoque massa, neque auctor leo dapibus urna dis cubilia nam, curae at est scelerisque habitasse ligula feugiat.</span>
                                 </div>
                             </div>
