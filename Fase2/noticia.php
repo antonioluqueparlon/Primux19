6cc37f27ecbbb6059b9bbad8503d7ddaaf3312c6
jesucr4 <jesucr4@users.noreply.github.com>
2020-12-01 13:02:39 +0100

    retoques 01/12

--- Fase2/noticia.php
+++ Fase2/noticia.php
@@ -8,55 +8,55 @@
 
 <?php include("includes/navigation.php");?>
 
-<main>
-<section class="content">
-		<div class="row introNoticia">
+<main >
+<section class="noticia">
+		<div class="row ">
 			<div class="row ">
-				<h1 >NOMBRE NOTICIA</h1>
+				<h1 class="titNoticia">NOMBRE NOTICIA</h1>
 			</div>
 			<div class="row">
-				<img src="http://lorempixel.com/output/sports-q-c-1150-900-3.jpg" alt="imgNoticia" >
+				<img src="http://lorempixel.com/output/sports-q-c-1150-800-7.jpg" alt="imgNoticia" >
 				<!--IMAGEN ANCHO DE 1150 ancho y 900 largo -->
 			</div>
 			<div class="row cuerpoNoticia">
-				<p>
+				<p >
 				Lorem ipsum dolor sit amet consectetur adipiscing elit integer per curae, sociosqu congue molestie penatibus non enim auctor feugiat interdum himenaeos, vitae metus aliquet parturient aliquam in semper fermentum arcu. Volutpat ac platea venenatis nisi sagittis ante mi neque, sollicitudin cursus litora class turpis potenti aliquet sem augue, condimentum urna egestas mauris curabitur luctus suspendisse. Semper urna non viverra fringilla leo suspendisse erat facilisi malesuada, 
 				auctor velit et bibendum dis feugiat porta aliquet commodo, vitae porttitor mi integer hac rhoncus fusce habitant. 
-				Odio fringilla sociis gravida viverra suspendisse est luctus phasellus, nulla ornare enim mattis ultrices primis eu sem, vitae hac himenaeos massa ultricies metus euismod.<br>
+				Odio fringilla sociis gravida viverra suspendisse est luctus phasellus, nulla ornare enim mattis ultrices primis eu sem, vitae hac himenaeos massa ultricies metus euismod.<br><br>
 				Semper venenatis quisque ad donec accumsan fames scelerisque congue duis facilisis, habitasse justo ultrices magna torquent litora parturient consequat id senectus, tellus vitae blandit aenean eleifend sagittis nibh elementum phasellus. 
-				Accumsan mauris integer varius sapien aenean imperdiet curabitur cubilia quis leo posuere lacus rutrum, primis suscipit nulla ante hac pellentesque sociosqu egestas pretium velit porttitor. Malesuada sociis fringilla dictum purus massa nibh, sagittis quam donec habitasse feugiat fames, congue eleifend et faucibus scelerisque.<br>
+				Accumsan mauris integer varius sapien aenean imperdiet curabitur cubilia quis leo posuere lacus rutrum, primis suscipit nulla ante hac pellentesque sociosqu egestas pretium velit porttitor. Malesuada sociis fringilla dictum purus massa nibh, sagittis quam donec habitasse feugiat fames, congue eleifend et faucibus scelerisque.<br><br>
 				Vel volutpat ultrices taciti sociis nulla mi euismod justo, eros bibendum imperdiet mus metus arcu cras eu, congue vitae rhoncus vestibulum ut lacus a. 
 				Ridiculus aptent aenean nunc cursus id netus arcu posuere metus mauris, tristique hac malesuada facilisis vitae at eleifend lectus semper, justo varius sollicitudin habitasse nec lobortis torquent convallis sapien. Tempor egestas enim facilisi venenatis montes mus convallis dictum vivamus gravida, sociosqu lobortis blandit congue proin posuere erat malesuada elementum eu, odio inceptos litora id tristique suspendisse suscipit dis nulla.<br>
 				</p>
 			</div>
-			<h2>Otras noticias</h2>
+			<h2 class="titNoticia">Otras noticias</h2>
 			<div class="row">
 				<div class="col-md-6">
 					<div class="row divOtrasNoticias">
 						<img src="http://placeimg.com/280/158/tech">
-						<p  class="txtOtrasNoticias">Lorem ipsum dolor sit amet consectetur.</p>				
+						<p  class="txtNoticias">Lorem ipsum dolor sit amet consectetur.</p>				
 					</div>
 				</div>
 				<div class="col-md-6">
 					<div class="row divOtrasNoticias">
 						<img src="http://placeimg.com/280/158/animals">
-						<p  class="txtOtrasNoticias">Lorem ipsum dolor sit amet consectetur.</p>				
+						<p  class="txtNoticias">Lorem ipsum dolor sit amet consectetur.</p>				
 					</div>
 				</div>
 				<div class="col-md-6">
 					<div class="row divOtrasNoticias">
 						<img src="http://placeimg.com/280/158/tech">
-						<p  class="txtOtrasNoticias">Lorem ipsum dolor sit amet consectetur.</p>				
+						<p  class="txtNoticias">Lorem ipsum dolor sit amet consectetur.</p>				
 					</div>
 				</div>
 				<div class="col-md-6">
 					<div class="row divOtrasNoticias">
 						<img src="http://placeimg.com/280/158/animals">
-						<p  class="txtOtrasNoticias">Lorem ipsum dolor sit amet consectetur.</p>				
+						<p  class="txtNoticias">Lorem ipsum dolor sit amet consectetur.</p>				
 					</div>
 				</div>	
 			</div>
-			<h2>Últimas noticias</h2>
+			<h2 class="titNoticia">Últimas noticias</h2>
 			<div class="row">
 			<div id="multi-item-example" class="carousel slide carousel-multi-item" data-ride="carousel">
 
@@ -79,7 +79,7 @@
 					<img class="card-img-top"
 					src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
 					<div class="card-body">
-					<h4 class="card-title">Card title</h4>
+					<h4 class="card-title titNoticia">Card title</h4>
 					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
 						card's content.</p>
 					<a class="btn btn-primary">Button</a>
@@ -92,7 +92,7 @@
 					<img class="card-img-top"
 					src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
 					<div class="card-body">
-					<h4 class="card-title">Card title</h4>
+					<h4 class="card-title titNoticia">Card title</h4>
 					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
 						card's content.</p>
 					<a class="btn btn-primary">Button</a>
@@ -105,7 +105,7 @@
 					<img class="card-img-top"
 					src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
 					<div class="card-body">
-					<h4 class="card-title">Card title</h4>
+					<h4 class="card-title titNoticia">Card title</h4>
 					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
 						card's content.</p>
 					<a class="btn btn-primary">Button</a>
@@ -118,7 +118,7 @@
 					<img class="card-img-top"
 					src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
 					<div class="card-body">
-					<h4 class="card-title">Card title</h4>
+					<h4 class="card-title titNoticia">Card title</h4>
 					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
 						card's content.</p>
 					<a class="btn btn-primary">Button</a>
@@ -190,7 +190,7 @@
 			<div class="col-12 ">
 				<form action="">
 					<div class="form-group">
-						<label for="textArea">Comentarios</label>
+						<label class="titNoticia" for="textArea">Comentarios</label>
 						
 						<textarea id="my-textarea" class="form-control" name="comentario" rows="3"></textarea>
 						
@@ -207,7 +207,7 @@
 					</div>
 					<div class="col col-sm-11 ">
 						<div class="row no-gutters">
-							<div class="col-12 ">Nombre
+							<div class="col-12 "><p class="titNoticia">Nombre</p>
 							</div>
 							<div class="col-12 ">Comentario
 							</div>
