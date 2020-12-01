be6f143183f9b9b33d29296f9c8d5a5e72111449
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2020-12-01 12:36:57 +0100

    Cambios en el estilo un poco//Recomendaciones del maestro

--- Fase2/includes/subNavigation.php
+++ Fase2/includes/subNavigation.php
@@ -1,14 +1,14 @@
-<ul class="nav justify-content-center">
+<ul class="nav justify-content-center subNav">
     <li class="nav-item">
-      <a class="nav-link" href="seccion.php">Subapartado1</a>
+      <a class="nav-link aSubNav" href="seccion.php">Subapartado1</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="seccion.php">Subapartado2</a>
+      <a class="nav-link aSubNav" href="seccion.php">Subapartado2</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="seccion.php">Subapartado3</a>
+      <a class="nav-link aSubNav" href="seccion.php">Subapartado3</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="seccion.php">Subapartado4</a>
+      <a class="nav-link aSubNav" href="seccion.php">Subapartado4</a>
     </li>
   </ul>
