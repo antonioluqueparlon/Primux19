5564028b2fc589e7ccf7571d7860ef61c018d1cb
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2020-12-03 14:18:07 +0100

    VERSION FINAL 03/12

--- Fase2/includes/head-tag-contents.php
+++ Fase2/includes/head-tag-contents.php
@@ -23,7 +23,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital@0;1&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="scss/mystyle.css">
+<link rel="stylesheet" href="css/mystyle.css">
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/index.css">
 <link rel="stylesheet" href="css/styleFooter.css">
