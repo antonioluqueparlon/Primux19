cdc8e6d282bb86640dbb3a104af5b19d6d33817c
jesucr4 <jesucr4@users.noreply.github.com>
2020-12-03 11:27:28 +0100

    noticia rehecha

--- Fase2/noticia.php
+++ Fase2/noticia.php
@@ -117,66 +117,125 @@
 			<button type="button" class="btn-primary btn-circleCarrusel" href="#multi-item-example" data-slide="next"><i class="fas fa-chevron-right"></i></button>
 			</div>
 
-			<!--Slides-->
-			<div class="carousel-inner" role="listbox">
+<!--Slides-->
+<div class="carousel-inner" role="listbox">
 
-			<!--First slide-->
-			<div class="carousel-item active">
+  <!--First slide-->
+  <div class="carousel-item active">
 
-				<div class="col-md-3" style="float:left">
-				<div class="card mb-2">
-					<img class="card-img-top"
-					src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
-					<div class="card-body">
-					<h4 class="card-title titNoticia">Card title</h4>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-						card's content.</p>
-					<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
-					</div>
-				</div>
-				</div>
+	<div class="col-md-3" style="float:left">
+	 <div class="card mb-2">
+		<img class="card-img-top"
+		  src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
+		<div class="card-body">
+		  <h4 class="card-title">Card title</h4>
+		  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+			card's content.</p>
+			<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
+		</div>
+	  </div>
+	</div>
 
-				<div class="col-md-3" style="float:left">
-				<div class="card mb-2">
-					<img class="card-img-top"
-					src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
-					<div class="card-body">
-					<h4 class="card-title titNoticia">Card title</h4>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-						card's content.</p>
-					<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
-					</div>
-				</div>
-				</div>
+	<div class="col-md-3" style="float:left">
+	  <div class="card mb-2">
+		<img class="card-img-top"
+		  src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
+		<div class="card-body">
+		  <h4 class="card-title">Card title</h4>
+		  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+			card's content.</p>
+			<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
+		</div>
+	  </div>
+	</div>
 
-				<div class="col-md-3" style="float:left">
-				<div class="card mb-2">
-					<img class="card-img-top"
-					src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
-					<div class="card-body">
-					<h4 class="card-title titNoticia">Card title</h4>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-						card's content.</p>
-					<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
-					</div>
-				</div>
-				</div>
-				
-				<div class="col-md-3" style="float:left">
-				<div class="card mb-2">
-					<img class="card-img-top"
-					src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
-					<div class="card-body">
-					<h4 class="card-title titNoticia">Card title</h4>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-						card's content.</p>
-					<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
-					</div>
-				</div>
-				</div>
+	<div class="col-md-3" style="float:left">
+	  <div class="card mb-2">
+		<img class="card-img-top"
+		  src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
+		<div class="card-body">
+		  <h4 class="card-title">Card title</h4>
+		  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+			card's content.</p>
+			<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
+		</div>
+	  </div>
+	</div>
+	
+	 <div class="col-md-3" style="float:left">
+	 <div class="card mb-2">
+		<img class="card-img-top"
+		  src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
+		<div class="card-body">
+		  <h4 class="card-title">Card title</h4>
+		  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+			card's content.</p>
+			<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
+		</div>
+	  </div>
+	</div>
+
+  </div>
+  <!--/.First slide-->
+
+  <!--Second slide-->
+  <div class="carousel-item">
+
+	<div class="col-md-3" style="float:left">
+	  <div class="card mb-2">
+		<img class="card-img-top"
+		  src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
+		<div class="card-body">
+		  <h4 class="card-title">Card title</h4>
+		  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+			card's content.</p>
+			<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
+		</div>
+	  </div>
+	</div>
+
+	<div class="col-md-3" style="float:left">
+	  <div class="card mb-2">
+		<img class="card-img-top"
+		  src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(47).jpg" alt="Card image cap">
+		<div class="card-body">
+		  <h4 class="card-title">Card title</h4>
+		  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+			card's content.</p>
+			<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
+		</div>
+	  </div>
+	</div>
+
+	<div class="col-md-3" style="float:left">
+	  <div class="card mb-2">
+		<img class="card-img-top"
+		  src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(48).jpg" alt="Card image cap">
+		<div class="card-body">
+		  <h4 class="card-title">Card title</h4>
+		  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+			card's content.</p>
+			<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
+		</div>
+	  </div>
+	</div>
+	
+	<div class="col-md-3" style="float:left">
+	  <div class="card mb-2">
+		<img class="card-img-top"
+		  src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(47).jpg" alt="Card image cap">
+		<div class="card-body">
+		  <h4 class="card-title">Card title</h4>
+		  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+			card's content.</p>
+			<a href="noticia.php" class="btn btn-primary linkNoticia">Ver más</a>
+		</div>
+	  </div>
+	</div>
 
   </div>
   <!--/.Second slide-->
+
 </div>
 <!--/.Slides-->
 </div>
