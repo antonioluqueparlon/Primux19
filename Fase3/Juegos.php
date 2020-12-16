52ab9d44fa108899f212736ae6eebecd234e474d
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2020-12-16 10:21:18 +0100

    Actualizacion

--- Fase3/Juegos.php
+++ Fase3/Juegos.php
@@ -20,11 +20,11 @@
       <div class="row justify-content-center">
         <div class="col-md-6">
           <h4>Parlón</h4>
-          <a href="/TechnoTeam/Fase3/juegos/parlon/index.php"><img width="50%" class="img-fluid" src="/TechnoTeam/Fase3/img/parlon.jpg"></a>
+          <a href="../Fase3/juegos/parlon/index.php"><img width="50%" class="img-fluid" src="../Fase3/img/parlon.jpg"></a>
         </div>
         <div class="col-md-6">
           <h4>Jesús Cano</h4>
-          <a href="/TechnoTeam/Fase3/juegos/jesus/index.php"><img width="50%" class="img-fluid" src="/TechnoTeam/Fase3/img/jesus.png"></a>
+          <a href="../Fase3/juegos/jesus/index.php"><img width="50%" class="img-fluid" src="../Fase3/img/jesus.png"></a>
         </div>
       </div>
 
@@ -33,11 +33,11 @@
       <div class="row justify-content-center">
         <div class="col-md-6">
           <h4>Irene</h4>
-          <a href="/TechnoTeam/Fase3/juegos/irene/index.php"><img width="50%" class="img-fluid" src="/TechnoTeam/Fase3/img/irene.jpg"></a>
+          <a href="../Fase3/juegos/irene/index.php"><img width="50%" class="img-fluid" src="../Fase3/img/irene.jpg"></a>
         </div>
         <div class="col-md-6">
           <h4>Antonio Pérez</h4>
-          <a href="/TechnoTeam/Fase3/juegos/antonioperez/index.php"><img width="50%" class="img-fluid" src="/TechnoTeam/Fase3/img/antonioperez.jpg"></a>
+          <a href="../Fase3/juegos/antonioperez/index.php"><img width="50%" class="img-fluid" src="../Fase3/img/antonioperez.jpg"></a>
         </div>
 
       </div>
