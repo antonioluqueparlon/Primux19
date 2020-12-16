b659e977111d1f8f6dd58401b3ab1218c747d191
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2020-12-16 10:09:32 +0100

    Arreglo de Juegos 16/12
    Listo para poner los juegos de todos

---
+++ Fase3/juegos/parlon/index.php
@@ -0,0 +1,26 @@
+<?php include("../includes/a_config.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <?php include("../includes/head-tag-contents.php"); ?>
+</head>
+
+<body>
+  <?php include("../includes/navigation.php"); ?>
+
+  <main>
+    <br>
+
+    <h1>PARLON JUEGO DRAGON BALL</h1><br><br>
+
+
+    
+  </main>
+
+  <!--Inclusion footer-->
+  <?php include("../includes/footer.php"); ?>
+
+</body>
+
+</html>
