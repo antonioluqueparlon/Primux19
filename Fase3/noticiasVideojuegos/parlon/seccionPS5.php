d452746cddfa6fb401d80a931255cb14e5d57d10
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2021-01-19 03:02:00 +0100

    3:01, he puesto una tonteria, me faltan cosas

---
+++ Fase3/noticiasVideojuegos/parlon/seccionPS5.php
@@ -0,0 +1,96 @@
+<?php include("../includes/a_config.php"); ?>
+<!DOCTYPE html>
+<html>
+<script>
+    
+</script>
+<head>
+    <?php include("../includes/head-tag-contents.php"); ?>
+</head>
+
+<body>
+
+    <!--<?php //include("includes/design-top.php"); 
+        ?>-->
+    <?php include("../includes/navigation.php"); ?>
+    <?php include("../includes/subNavigationVideojuegos.php"); ?>
+
+
+    <main>
+        <section class="ultimasNoticias claseSeccion">
+
+            <h1>PLAYSTATION </h1>
+
+            <div class="card cardArticulo">
+                <a href="noticia.php" class="enlaceNoticia">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="https://m.eltiempo.com/uploads/2019/12/07/5dec47012d257.jpeg" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h4 class="card-title indexNoticiaTitulo">Título de noticia</h4>
+                                <p class="card-text indexNoticiaTimer"><small class="text-muted">Last updated 14 mins ago</small></p>
+                                <p class="card-text indexNoticiaTexto">Lorem ipsum dolor sit amet consectetur adipiscing elit molestie erat vel aliquam duis lacinia torquent gravida, vivamus inceptos nascetur justo enim augue fringilla luctus lectus sociosqu dignissim blandit hac rutrum.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="card cardArticulo">
+                <a href="noticia.php" class="enlaceNoticia">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="https://www.redeszone.net/app/uploads-redeszone.net/2019/12/novedades-redes-2020.jpg" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h4 class="card-title indexNoticiaTitulo">Título de noticia</h4>
+                                <p class="card-text indexNoticiaTimer"><small class="text-muted">Last updated 36 mins ago</small></p>
+                                <p class="card-text indexNoticiaTexto">Lorem ipsum dolor sit amet consectetur adipiscing elit molestie erat vel aliquam duis lacinia torquent gravida, vivamus inceptos nascetur justo enim augue fringilla luctus lectus sociosqu dignissim blandit hac rutrum.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="card cardArticulo">
+                <a href="noticia.php" class="enlaceNoticia">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="https://ichef.bbci.co.uk/news/640/cpsprodpb/12274/production/_96065347_gettyimages-482855185.jpg" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h4 class="card-title indexNoticiaTitulo">Título de noticia</h4>
+                                <p class="card-text indexNoticiaTimer"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                <p class="card-text indexNoticiaTexto">Lorem ipsum dolor sit amet consectetur adipiscing elit molestie erat vel aliquam duis lacinia torquent gravida, vivamus inceptos nascetur justo enim augue fringilla luctus lectus sociosqu dignissim blandit hac rutrum.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="card cardArticulo">
+                <a href="noticia.php" class="enlaceNoticia">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="https://as01.epimg.net/meristation/imagenes/2020/03/02/betech/1583161547_749146_1583161917_noticia_normal_recorte1.jpg" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h4 class="card-title indexNoticiaTitulo">Título de noticia</h4>
+                                <p class="card-text indexNoticiaTimer"><small class="text-muted">Last updated 54 mins ago</small></p>
+                                <p class="card-text indexNoticiaTexto">Lorem ipsum dolor sit amet consectetur adipiscing elit molestie erat vel aliquam duis lacinia torquent gravida, vivamus inceptos nascetur justo enim augue fringilla luctus lectus sociosqu dignissim blandit hac rutrum.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </section>
+
+    </main>
+
+    <?php include("../includes/footer.php"); ?>
+
+</body>
+
+</html>
