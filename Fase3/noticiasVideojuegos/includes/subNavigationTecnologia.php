eec7a342562f10a0dbc63efda40bdecdad27e719
irenelolie <irenelopezliebana@gmail.com>
2021-01-20 23:55:41 +0100

    subNavigationTecnologia Irene 23:55

--- Fase3/noticiasVideojuegos/includes/subNavigationTecnologia.php
+++ Fase3/noticiasVideojuegos/includes/subNavigationTecnologia.php
@@ -2,16 +2,16 @@
   <div class>
     <ul class="nav justify-content-center subNav">
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="../secciones/seccionMovil.php">MÓVILES</a>
+        <a class="nav-link aSubNav" href="../../noticiasTecnologia/secciones/seccionMovil.php">MÓVILES</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="../secciones/seccionWindows.php">WINDOWS</a>
+        <a class="nav-link aSubNav" href="../../noticiasTecnologia/secciones/seccionWindows.php">WINDOWS</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="../secciones/seccionLinux.php">LINUX</a>
+        <a class="nav-link aSubNav" href="../../noticiasTecnologia/secciones/seccionLinux.php">LINUX</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="../secciones/seccionOtros.php">OTRAS NOTICIAS</a>
+        <a class="nav-link aSubNav" href="../../noticiasTecnologia/secciones/seccionOtros.php">OTRAS NOTICIAS</a>
       </li>
     </ul>
   </div>
