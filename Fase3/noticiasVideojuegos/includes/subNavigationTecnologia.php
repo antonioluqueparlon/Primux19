927ef1ca9525d9b0426c6f184791eb8436e9771d
irenelolie <irenelopezliebana@gmail.com>
2021-01-20 23:39:39 +0100

    modificaciones apartadoTecnologia irene

--- Fase3/noticiasVideojuegos/includes/subNavigationTecnologia.php
+++ Fase3/noticiasVideojuegos/includes/subNavigationTecnologia.php
@@ -2,16 +2,16 @@
   <div class>
     <ul class="nav justify-content-center subNav">
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="../../seccion.php">INFORMATICA</a>
+        <a class="nav-link aSubNav" href="../secciones/seccionMovil.php">MÓVILES</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="../../seccion.php">WINDOWS</a>
+        <a class="nav-link aSubNav" href="../secciones/seccionWindows.php">WINDOWS</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="../../seccion.php">LINUX</a>
+        <a class="nav-link aSubNav" href="../secciones/seccionLinux.php">LINUX</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="seccion.php">OTRAS NOTICIAS</a>
+        <a class="nav-link aSubNav" href="../secciones/seccionOtros.php">OTRAS NOTICIAS</a>
       </li>
     </ul>
   </div>
