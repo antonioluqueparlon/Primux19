f8e2f7ee5bb304208a82b13baf99b26cf953f05b
AntonioJesPG <antoniojespg@gmail.com>
2021-01-21 02:52:23 +0100

    noticiasMotor

--- Fase3/apartadoMotor.php
+++ Fase3/apartadoMotor.php
@@ -17,13 +17,13 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 col-md-12">
-                        <h1>APARTADO</h1>
+                        <h1>Lo más visto</h1>
                         <div class="card img-fluid">
-                            <a href="noticia.php">
+                            <a href="../Fase3/noticiasMotor/subseccionFormula1/n1F1.php">
                                 <img class="card-img-top img-fluid"
-                                    src="http://lorempixel.com/output/sports-q-c-320-180-9.jpg" alt="Card image">
+                                    src="../Fase3/noticiasMotor/img/sainzFerrari.jpg" alt="Card image">
                                 <div class="card-img-overlay d-flex align-items-end">
-                                    <p class="card-text font-weight-bold txtTop">Lorem ipsum dolor sit amet</p>
+                                    <p class="card-text font-weight-bold txtTop">"No tengo dudas: Sainz tendrá éxito en Ferrari"</p>
                                 </div>
                             </a>
                         </div>
@@ -34,10 +34,10 @@
                         <h1>Últimas noticias</h1>
                         <div class="row">
                             <div class="col-md-12">
-                                <a href="noticia.php" class="linkNoticia">
+                                <a href="../Fase3/noticiasMotor/subseccionFormula1/n2F1.php" class="linkNoticia">
                                     <div class="card bg-light cardApartadoDerecha">
                                         <div class="card-body text-center">
-                                            <p class="card-text">ASKHAPSGIHWOGOGWPONG</p>
+                                            <p class="card-text">Mazepin, consciente de que tendrá "un solo disparo en la F1"</p>
                                         </div>
                                     </div>
                                 </a>
@@ -45,10 +45,10 @@
                         </div>
                         <div class="row">
                             <div class="col-md-12">
-                                <a href="noticia.php" class="linkNoticia">
+                                <a href="../Fase3/noticiasMotor/subseccionFormula1/n3F1.php" class="linkNoticia">
                                     <div class="card bg-light cardApartadoDerecha">
                                         <div class="card-body text-center">
-                                            <p class="card-text">ASKHAPSGIHWOGOGWPONG</p>
+                                            <p class="card-text">La renovación de Hamilton</p>
                                         </div>
                                     </div>
                                 </a>
@@ -56,10 +56,10 @@
                         </div>
                         <div class="row">
                             <div class="col-md-12">
-                                <a href="noticia.php" class="linkNoticia">
+                                <a href="../Fase3/noticiasMotor/subseccionFormula1/n4F1.php" class="linkNoticia">
                                     <div class="card bg-light cardApartadoDerecha">
                                         <div class="card-body text-center">
-                                            <p class="card-text">ASKHAPSGIHWOGOGWPONG</p>
+                                            <p class="card-text">El nuevo motor del Ferrari SF21</p>
                                         </div>
                                     </div>
                                 </a>
@@ -67,10 +67,11 @@
                         </div>
                         <div class="row">
                             <div class="col-md-12 d-lg-none d-xl-block">
-                                <a href="noticia.php" class="linkNoticia">
+                                <a href="../Fase3/noticiasMotor/subseccionFormulaE/n1FE.php" class="linkNoticia">
                                     <div class="card bg-light cardApartadoDerecha">
                                         <div class="card-body text-center">
-                                            <p class="card-text">ASKHAPSGIHWOGOGWPONG</p>
+                                            <p class="card-text">La salida de Audi y BMW de la Fórmula E abre nuevas puertas
+</p>
                                         </div>
                                     </div>
                                 </a>
@@ -78,10 +79,10 @@
                         </div>
                         <div class="row">
                             <div class="col-md-12 d-lg-none d-xl-block">
-                                <a href="noticia.php" class="linkNoticia">
+                                <a href="../Fase3/noticiasMotor/subseccionFormulaE/n2FE.php" class="linkNoticia">
                                     <div class="card bg-light cardApartadoDerecha">
                                         <div class="card-body text-center">
-                                            <p class="card-text">ASKHAPSGIHWOGOGWPONG</p>
+                                            <p class="card-text">Coronavirus: eventos cancelados</p>
                                         </div>
                                     </div>
                                 </a>
@@ -148,60 +149,54 @@
 
                     <!--TARJETAS DE NOTICIAS -->
                     <div class="card cardArticulo tarjetaNoticiaApartado">
-                        <a href="noticia.php" class="enlaceNoticia">
+                        <a href="../Fase3/noticiasMotor/subseccionRally/n1R.php" class="enlaceNoticia">
                             <div class="row no-gutters">
                                 <div class="col-md-12 col-lg-4">
-                                    <img src="https://as01.epimg.net/meristation/imagenes/2020/06/17/betech/1592423233_521074_1592423565_noticia_normal_recorte1.jpg"
+                                    <img src="../Fase3/noticiasMotor/img/wrc400.jpg"
                                         class="card-img" alt="...">
                                 </div>
                                 <div class="col-md-12 col-lg-8">
                                     <div class="card-body">
-                                        <h3 class="card-title indexNoticiaTitulo">Card title</h3>
+                                        <h3 class="card-title indexNoticiaTitulo">WRC: Bottas quiere correr el Rally del Ártico tras acabar sexto en Finlandia</h3>
                                         <p class="card-text indexNoticiaTimer"><small class="text-muted">Last updated 3
                                                 mins ago</small></p>
-                                        <p class="card-text indexNoticiaTexto">This is a wider card with supporting text
-                                            below as a natural lead-in to additional content. This content is a little
-                                            bit longer.</p>
+                                        <p class="card-text indexNoticiaTexto">Valtteri Bottas finalizó sexto en el Rally del Ártico en Finlandia que formará parte del Mundial en 2021, y no descarta competir en él ya que la pretemporada de F1 no arrancará hasta marzo.</p>
                                     </div>
                                 </div>
                             </div>
                         </a>
                     </div>
                     <div class="card cardArticulo tarjetaNoticiaApartado">
-                        <a href="noticia.php" class="enlaceNoticia ">
+                        <a href="../Fase3/noticiasMotor/subseccionRally/n3R.php" class="enlaceNoticia ">
                             <div class="row no-gutters">
                                 <div class="col-md-12 col-lg-4">
-                                    <img src="https://as01.epimg.net/meristation/imagenes/2020/06/17/betech/1592423233_521074_1592423565_noticia_normal_recorte1.jpg"
+                                    <img src="../Fase3/noticiasMotor/img/naniRoma400.jpg"
                                         class="card-img" alt="...">
                                 </div>
                                 <div class="col-md-12 col-lg-8">
                                     <div class="card-body">
-                                        <h3 class="card-title indexNoticiaTitulo">Card title</h3>
+                                        <h3 class="card-title indexNoticiaTitulo">Nani Roma, 5º en su 25º Dakar: "Volveremos más fuertes en 2022"</h3>
                                         <p class="card-text indexNoticiaTimer"><small class="text-muted">Last updated 3
                                                 mins ago</small></p>
-                                        <p class="card-text indexNoticiaTexto">This is a wider card with supporting text
-                                            below as a natural lead-in to additional content. This content is a little
-                                            bit longer.</p>
+                                        <p class="card-text indexNoticiaTexto">Nani Roma y Alex Winocq lograron cerrar el top 5 del Dakar 2021 en coches en el estreno del equipo Bahrain Raid Xtreme y su BRX T1.</p>
                                     </div>
                                 </div>
                             </div>
                         </a>
                     </div>
                     <div class="card cardArticulo tarjetaNoticiaApartado">
-                        <a href="noticia.php" class="enlaceNoticia">
+                        <a href="../Fase3/noticiasMotor/subseccionRally/n4R.php" class="enlaceNoticia">
                             <div class="row no-gutters">
                                 <div class="col-md-12 col-lg-4">
-                                    <img src="https://as01.epimg.net/meristation/imagenes/2020/06/17/betech/1592423233_521074_1592423565_noticia_normal_recorte1.jpg"
+                                    <img src="../Fase3/noticiasMotor/img/dakarJuego400.jpg"
                                         class="card-img" alt="...">
                                 </div>
                                 <div class="col-md-12 col-lg-8">
                                     <div class="card-body">
-                                        <h3 class="card-title indexNoticiaTitulo">Card title</h3>
+                                        <h3 class="card-title indexNoticiaTitulo">El Dakar tendrá nuevo videojuego: primeros detalles</h3>
                                         <p class="card-text indexNoticiaTimer"><small class="text-muted">Last updated 3
                                                 mins ago</small></p>
-                                        <p class="card-text indexNoticiaTexto">This is a wider card with supporting text
-                                            below as a natural lead-in to additional content. This content is a little
-                                            bit longer.</p>
+                                        <p class="card-text indexNoticiaTexto">El estudio portugués Saber Porto –ex BigMoon– está desarrollando un nuevo videojuego oficial de mundo abierto del Rally Dakar con muchas novedades.</p>
                                     </div>
                                 </div>
                             </div>
--- Fase3/noticiasMotor/includes/subNavigationMotor.php
+++ Fase3/noticiasMotor/includes/subNavigationMotor.php
@@ -2,18 +2,19 @@
   <div class>
     <ul class="nav justify-content-center subNav">
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="../../seccion.php">FORMULA 1</a>
+        <a class="nav-link aSubNav" href="../subseccionFormula1/seccionF1.php">FORMULA 1</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="../../seccion.php">RALLY</a>
+        <a class="nav-link aSubNav" href="../subseccionRally/seccionR.php">RALLY</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="../../seccion.php">MOTOGP</a>
+        <a class="nav-link aSubNav" href="../subseccionMotoGP/seccionGP.php">MOTOGP</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link aSubNav" href="../../seccion.php">FORMULA E</a>
+        <a class="nav-link aSubNav" href="../subseccionFormulaE/seccionFE.php">FORMULA E</a>
       </li>
     </ul>
   </div>
+  
 
 
