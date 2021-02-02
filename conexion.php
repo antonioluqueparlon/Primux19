948132f7a089a671eb91a4915f5b058270addac2
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2021-02-02 14:22:00 +0100

    prueba conexion

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = mysqli_connect('localhost', 'root', '', 'technoteam');
+    $conn = mysqli_connect('localhost', 'root', 'cfgsdaw1', 'technoteam');
     if(!$conn){
         echo 'Error en la base datos';
     }
