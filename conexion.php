21f71976fb43ce1d42540ee794809671880d72df
antonioluqueparlon <55851762+antonioluqueparlon@users.noreply.github.com>
2021-02-04 11:24:48 +0100

    11:24

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = mysqli_connect('technoteam.iesmarquesdecomares.org', 'root', '', 'technoteam');
+    $conn = mysqli_connect('localhost', 'prueba', 'Password123#@!', 'technoteam');
     if(!$conn){
         echo 'Error en la base datos';
     }
